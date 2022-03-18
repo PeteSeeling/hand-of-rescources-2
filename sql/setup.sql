cb17af031ac005e977687f475e8385415e5a9188
Pete Seeling <peteseeling@gmail.com>
2022-03-18 13:35:38 -0600

    dogs sql table made

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS dogs;
 
 CREATE TABLE movies (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,10 +10,24 @@ CREATE TABLE movies (
     star TEXT NOT NULL,
     years BIGINT
 )
+CREATE TABLE dogs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    breed TEXT NOT NULL,
+    named TEXT NOT NULL,
+    age BIGINT
+)
 
 INSERT INTO
     movies (title, star, years)
+  
 
 VALUES
     ('Training Day', 'Denzel Washington', '2004'),
     ('Man on Fire', 'Denzel Washington', '2001');
+
+INSERT INTO
+    dogs (breed, named, age)
+
+VALUES
+    ('newfoundland', 'Chelsea','12')
+    ('lab', 'Bailey', '10')
