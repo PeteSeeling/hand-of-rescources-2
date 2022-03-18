971d792f11a9a637ade01b85b241e3645276d7ac
Pete Seeling <peteseeling@gmail.com>
2022-03-18 16:57:33 -0600

    dogs test passing

--- sql/setup.sql
+++ sql/setup.sql
@@ -8,14 +8,14 @@ CREATE TABLE movies (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
     star TEXT NOT NULL,
-    years BIGINT
-)
+    years INT
+);
 CREATE TABLE dogs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     breed TEXT NOT NULL,
     named TEXT NOT NULL,
-    age BIGINT
-)
+    age INT
+);
 
 INSERT INTO
     movies (title, star, years)
@@ -29,5 +29,5 @@ INSERT INTO
     dogs (breed, named, age)
 
 VALUES
-    ('newfoundland', 'Chelsea','12')
-    ('lab', 'Bailey', '10')
+    ('newfoundland', 'Chelsea','12'),
+    ('lab', 'Bailey', '10');
