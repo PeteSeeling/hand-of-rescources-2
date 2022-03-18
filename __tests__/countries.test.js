a1a13e9f7ab82a634297cab2d67b85cb9d51abc1
Pete Seeling <peteseeling@gmail.com>
2022-03-18 17:39:57 -0600

    create countries written

---
+++ __tests__/countries.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Country = require('../lib/models/Country');
+
+describe('hand-of-rescources-2 routes', () => {
+    beforeEach(() => {
+      return setup(pool);
+    });
+  
+    afterAll(() => {
+      pool.end();
+    });
+
+    it('creates a country', async () => {
+        const expected = {
+          named: 'Nicaragua', 
+          borders:'Costa Rica',
+          founded:1821
+        };
+        const res = await (await request(app).post('/api/v1/countries')).setEncoding(expected);
+
+        expect(res.body).toEqual({ id: expect.any(String), ...expected });
+    });
